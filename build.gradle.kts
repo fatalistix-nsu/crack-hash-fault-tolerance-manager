fa7868a86cb5eca51ab6c0564e166560db3b774c
Viacheslav Balashov <slavabal11@outlook.com>
2025-02-27 21:53:17 +0700

    gradle infrastructure

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+val koin_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "3.0.3"
+}
+
+group = "com.github.fatalistix"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("dev.hayden:khealth:3.0.1")
+    implementation("io.ktor:ktor-client-cio")
+    implementation("io.ktor:ktor-client-content-negotiation")
+    implementation("io.ktor:ktor-client-core")
+    implementation("io.ktor:ktor-server-call-logging")
+    implementation("io.ktor:ktor-server-call-id")
+    implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-default-headers")
+    implementation("io.ktor:ktor-server-metrics")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-request-validation")
+    implementation("io.ktor:ktor-server-swagger")
+    implementation("io.ktor:ktor-serialization-jackson")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
