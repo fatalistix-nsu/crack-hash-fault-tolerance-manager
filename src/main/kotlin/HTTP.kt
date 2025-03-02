b60740df4c86673dc5ebe510405d4774b268d38f
Viacheslav Balashov <slavabal11@outlook.com>
2025-03-03 00:43:03 +0700

    added exception handler

--- src/main/kotlin/HTTP.kt
+++ src/main/kotlin/HTTP.kt
@@ -1,10 +1,23 @@
 package com.github.fatalistix
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.requestvalidation.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
 
 fun Application.configureHTTP() {
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
+    }
+    install(StatusPages) {
+        exception<RequestValidationException> { call, cause ->
+            call.respond(HttpStatusCode.UnprocessableEntity, cause.reasons)
+        }
+        exception<NotFoundException> { call, cause ->
+            call.respondText(status = HttpStatusCode.NotFound, text = cause.message.orEmpty())
+        }
     }
 }
