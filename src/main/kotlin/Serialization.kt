38ce03f269fd625892d674605fcd2845391e2c1d
Viacheslav Balashov <slavabal11@outlook.com>
2025-04-04 19:01:15 +0700

    kotlin module usage

--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -3,6 +3,7 @@ package com.github.fatalistix
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
@@ -10,6 +11,7 @@ import io.ktor.server.plugins.contentnegotiation.*
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         jackson {
+            registerModule(kotlinModule())
             enable(SerializationFeature.INDENT_OUTPUT)
             configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
