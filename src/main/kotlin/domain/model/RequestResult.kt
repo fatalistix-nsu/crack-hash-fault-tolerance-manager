965ef2370eb965e9550eed4f075d19497e7a7a28
Viacheslav Balashov <slavabal11@outlook.com>
2025-04-06 17:48:58 +0700

    made pure model

--- src/main/kotlin/domain/model/RequestResult.kt
+++ src/main/kotlin/domain/model/RequestResult.kt
@@ -1,11 +1,7 @@
 package com.github.fatalistix.domain.model
 
-import org.bson.codecs.pojo.annotations.BsonId
-import org.bson.types.ObjectId
-
 data class RequestResult(
-    @BsonId
-    val requestId: ObjectId = ObjectId(),
+    val requestId: String,
     var status: RequestStatus = RequestStatus.IN_PROGRESS,
     val data: MutableSet<String> = mutableSetOf(),
 )
