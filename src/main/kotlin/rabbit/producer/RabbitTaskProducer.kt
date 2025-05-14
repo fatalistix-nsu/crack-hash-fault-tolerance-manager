3370bbddb8c86920c1b920b117650364511179de
Viacheslav Balashov <slavabal11@outlook.com>
2025-05-15 01:37:34 +0700

    connection manager usage

--- src/main/kotlin/rabbit/producer/RabbitTaskProducer.kt
+++ src/main/kotlin/rabbit/producer/RabbitTaskProducer.kt
@@ -6,30 +6,36 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
 import com.github.fatalistix.domain.model.Task
-import com.github.fatalistix.domain.model.Worker
+import com.github.fatalistix.rabbit.RabbitConnectionManager
 import com.rabbitmq.client.AMQP.BasicProperties
-import com.rabbitmq.client.Channel
-import io.ktor.util.logging.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
+import org.slf4j.LoggerFactory
 import kotlin.time.Duration
 
 private data class RabbitTaskMessage(
+    val id: String,
     val requestId: String,
-    val taskId: String,
     val alphabet: String,
     val hash: String,
-    val maxLength: ULong,
-    val start: ULong,
-    val end: ULong,
+    val maxLength: Long,
+    val start: Long,
+    val end: Long,
 )
 
 class RabbitTaskProducer(
-    private val channel: Channel,
+    manager: RabbitConnectionManager,
+    private val publishTimeout: Duration,
     private val exchangeName: String,
-    private val messageExpiration: Duration,
-    private val log: Logger,
 ) {
+    companion object {
+        private val log = LoggerFactory.getLogger(RabbitTaskProducer::class.java)!!
+    }
+
+    private val channelFlow = manager.channelFlow
+
     private val objectMapper = jacksonObjectMapper().apply {
         registerModule(kotlinModule())
         enable(SerializationFeature.INDENT_OUTPUT)
@@ -37,23 +43,27 @@ class RabbitTaskProducer(
         propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
     }
 
-    suspend fun send(taskId: String, task: Task, worker: Worker): Result<Unit> {
-        val message = task.toMessage(taskId)
+    suspend fun produce(task: Task) = runCatching {
+        val message = task.toMessage()
         val jsonMessage = objectMapper.writeValueAsString(message)
         log.debug("Sending message: {}", jsonMessage)
 
-        val props = BasicProperties.Builder().apply {
-            expiration(messageExpiration.inWholeMilliseconds.toString())
-        }.build()
-
-        return runCatching {
-            withContext(Dispatchers.IO) {
-                channel.basicPublish(exchangeName, worker.id, props, jsonMessage.toByteArray())
+        withTimeout(publishTimeout) {
+            while (true) {
+                val channel = channelFlow.first() ?: continue
+                try {
+                    withContext(Dispatchers.IO) {
+                        channel.basicPublish(exchangeName, "", null, jsonMessage.toByteArray())
+                    }
+                    return@withTimeout
+                } catch (e: Exception) {
+                    log.error("Error during sending task [{}]", task.id, e)
+                }
             }
         }
     }
 }
 
-private fun Task.toMessage(taskId: String) = RabbitTaskMessage(
-    requestId, taskId, alphabet, hash, maxLength, start, end,
+private fun Task.toMessage() = RabbitTaskMessage(
+    id, requestId, alphabet, hash, maxLength, start, end,
 )
