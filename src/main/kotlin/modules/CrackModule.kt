12266b52b95209c831a4add861c72f902cf694bf
Viacheslav Balashov <slavabal11@outlook.com>
2025-05-15 01:36:00 +0700

    removed classes, added variables

--- src/main/kotlin/modules/CrackModule.kt
+++ src/main/kotlin/modules/CrackModule.kt
@@ -1,25 +1,24 @@
 package com.github.fatalistix.modules
 
+import com.github.fatalistix.mongo.repository.RequestResultRepository
+import com.github.fatalistix.mongo.repository.TaskRepository
+import com.github.fatalistix.rabbit.RabbitConnectionManager
 import com.github.fatalistix.rabbit.consumer.RabbitResultConsumer
 import com.github.fatalistix.rabbit.producer.RabbitTaskProducer
 import com.github.fatalistix.services.CrackService
-import com.github.fatalistix.services.WorkerPool
 import com.github.fatalistix.services.execution.ActorManager
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import com.mongodb.WriteConcern
 import com.mongodb.kotlin.client.MongoClient
-import com.rabbitmq.client.ConnectionFactory
 import io.ktor.server.config.*
-import io.ktor.util.logging.*
 import org.koin.dsl.module
 import java.util.concurrent.TimeUnit
 import kotlin.time.Duration
 
-fun crackModule(config: ApplicationConfig, log: Logger) = module {
+fun crackModule(config: ApplicationConfig) = module {
     val actorCount = config.property("application.actor.count").getString().toInt()
-    val workerResponseTimeout = Duration.parse(config.property("application.worker.response.timeout").getString())
-    val workerAcquireTimeout = Duration.parse(config.property("application.worker.acquire.timeout").getString())
+    val taskSize = config.property("application.task.size").getString().toLong()
     val mongoConnection = config.property("application.mongo.connection").getString()
     val mongoDatabase = config.property("application.mongo.database").getString()
     val mongoWriteTimeoutDuration = Duration.parse(config.property("application.mongo.write.timeout").getString())
@@ -28,8 +27,9 @@ fun crackModule(config: ApplicationConfig, log: Logger) = module {
     val rabbitMqPort = config.property("application.rabbitmq.port").getString().toInt()
     val rabbitMqUsername = config.property("application.rabbitmq.username").getString()
     val rabbitMqPassword = config.property("application.rabbitmq.password").getString()
+    val rabbitMqPublishTimeout = Duration.parse(config.property("application.rabbitmq.publish.timeout").getString())
+    val rabbitMqReconnectDelay = Duration.parse(config.property("application.rabbitmq.reconnect.delay").getString())
     val rabbitMqTaskExchangeName = config.property("application.rabbitmq.task.exchange.name").getString()
-    val rabbitMqTaskMessageExpiration = Duration.parse(config.property("application.rabbitmq.task.message.expiration").getString())
     val rabbitMqResultExchangeName = config.property("application.rabbitmq.result.exchange.name").getString()
 
     val mongoWriteConcern = WriteConcern.MAJORITY
@@ -44,51 +44,57 @@ fun crackModule(config: ApplicationConfig, log: Logger) = module {
     val mongoClient = MongoClient.create(mongoSettings)
     val database = mongoClient.getDatabase(mongoDatabase)
 
-    val rabbitMqFactory = ConnectionFactory().apply {
-        host = rabbitMqHost
-        port = rabbitMqPort
-        username = rabbitMqUsername
-        password = rabbitMqPassword
-    }
-    val rabbitMqConnection = rabbitMqFactory.newConnection()!!
-    val rabbitMqChannel = rabbitMqConnection.createChannel()!!
-
-    single<CrackService> {
-        CrackService(
-            actorManager = get(),
-            log = log,
+    single<RequestResultRepository> {
+        RequestResultRepository(
             database = database,
         )
     }
 
-    single<WorkerPool> { WorkerPool() }
+    single<TaskRepository> {
+        TaskRepository(
+            database = database,
+        )
+    }
 
+    single<CrackService> {
+        CrackService(
+            actorManager = get(),
+            repository = get(),
+        )
+    }
     single<ActorManager> {
         ActorManager(
             producer = get(),
-            workerPool = get(),
-            count = actorCount,
-            workerResponseTimeout = workerResponseTimeout,
-            workerAcquireTimeout = workerAcquireTimeout,
-            log = log,
+            taskRepository = get(),
+            requestResultRepository = get(),
+            taskSize = taskSize,
+            actorCount = actorCount,
+        )
+    }
+
+    single<RabbitConnectionManager> {
+        RabbitConnectionManager(
+            host = rabbitMqHost,
+            port = rabbitMqPort,
+            username = rabbitMqUsername,
+            password = rabbitMqPassword,
+            reconnectDelay = rabbitMqReconnectDelay,
         )
     }
 
     single<RabbitTaskProducer> {
         RabbitTaskProducer(
-            channel = rabbitMqChannel,
+            manager = get(),
+            publishTimeout = rabbitMqPublishTimeout,
             exchangeName = rabbitMqTaskExchangeName,
-            messageExpiration = rabbitMqTaskMessageExpiration,
-            log = log,
         )
     }
 
     single<RabbitResultConsumer> {
         RabbitResultConsumer(
-            channel = rabbitMqChannel,
-            exchangeName = rabbitMqResultExchangeName,
+            connectionManager = get(),
             actorManager = get(),
-            workerPool = get(),
+            exchangeName = rabbitMqResultExchangeName,
         )
     }
 }
