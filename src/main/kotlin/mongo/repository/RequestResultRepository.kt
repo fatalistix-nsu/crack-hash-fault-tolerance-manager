d579d41e715df9e995af5a7542c2a890e7cd2b36
Viacheslav Balashov <slavabal11@outlook.com>
2025-05-15 01:36:17 +0700

    extended model, more methods

--- src/main/kotlin/mongo/repository/RequestResultRepository.kt
+++ src/main/kotlin/mongo/repository/RequestResultRepository.kt
@@ -1,5 +1,6 @@
 package com.github.fatalistix.mongo.repository
 
+import com.github.fatalistix.domain.model.Request
 import com.github.fatalistix.domain.model.RequestResult
 import com.github.fatalistix.domain.model.RequestStatus
 import com.mongodb.client.model.Filters.eq
@@ -9,20 +10,36 @@ import com.mongodb.kotlin.client.MongoDatabase
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
 
-private data class RequestResultEntity(
+data class RequestResultEntity(
     @BsonId
-    val requestId: ObjectId = ObjectId(),
-    var status: RequestStatus = RequestStatus.IN_PROGRESS,
+    val id: ObjectId = ObjectId(),
+    val status: RequestStatus = RequestStatus.IN_PROGRESS,
     val data: MutableSet<String> = mutableSetOf(),
-)
+    val alphabet: String,
+    val hash: String,
+    val maxLength: Long,
+) {
+    constructor(
+        alphabet: String,
+        hash: String,
+        maxLength: Long,
+    ) : this(
+        ObjectId(),
+        RequestStatus.IN_PROGRESS,
+        mutableSetOf(),
+        alphabet,
+        hash,
+        maxLength,
+    )
+}
 
 class RequestResultRepository(
     database: MongoDatabase,
 ) {
     private val collection = database.getCollection<RequestResultEntity>("request_results")
 
-    fun insert(): RequestResult {
-        val entity = RequestResultEntity()
+    fun insert(alphabet: String, hash: String, maxLength: Long): RequestResult {
+        val entity = RequestResultEntity(alphabet, hash, maxLength)
         collection.insertOne(entity)
         return entity.toModel()
     }
@@ -48,8 +65,28 @@ class RequestResultRepository(
             set("status", status),
         )
     }
+
+    fun getAllInProgress(): List<RequestResult> {
+        val e = collection.find(
+            eq("status", RequestStatus.IN_PROGRESS),
+        )
+
+        return e.map { it.toModel() }.toList()
+    }
+
+    fun getAllReady(): List<RequestResult> {
+        val e = collection.find(
+            eq("status", RequestStatus.READY),
+        )
+
+        return e.map { it.toModel() }.toList()
+    }
 }
 
 private fun RequestResultEntity.toModel() = RequestResult(
-    requestId.toHexString()!!, status, data.toMutableSet()
+    status, data.toMutableSet(), this.toRequest()
+)
+
+private fun RequestResultEntity.toRequest() = Request(
+    id.toHexString()!!, alphabet, hash, maxLength
 )
