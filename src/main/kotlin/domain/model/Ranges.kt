266d7e4b1dafed35eb66a6d3cb5c2650a69be48e
Viacheslav Balashov <slavabal11@outlook.com>
2025-05-15 01:34:02 +0700

    removed unnecessary dto

--- src/main/kotlin/domain/model/Ranges.kt
+++
@@ -1,7 +0,0 @@
-package com.github.fatalistix.domain.model
-
-import java.util.*
-
-data class Ranges(
-    val ranges: MutableSet<Pair<ULong, ULong>> = TreeSet<Pair<ULong, ULong>>(compareBy { it.first })
-)
