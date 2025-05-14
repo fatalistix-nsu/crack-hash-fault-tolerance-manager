4e648c91c77d2893d671a2c2b5860528acad009f
Viacheslav Balashov <slavabal11@outlook.com>
2025-05-15 01:32:40 +0700

    added connection manager startup and finish

--- src/main/kotlin/Rabbit.kt
+++ src/main/kotlin/Rabbit.kt
@@ -1,17 +1,21 @@
 package com.github.fatalistix
 
+import com.github.fatalistix.rabbit.RabbitConnectionManager
 import com.github.fatalistix.rabbit.consumer.RabbitResultConsumer
 import io.ktor.server.application.*
 import org.koin.ktor.ext.inject
 
 fun Application.configureRabbit() {
+    val connectionManager: RabbitConnectionManager by inject()
     val resultConsumer: RabbitResultConsumer by inject()
 
     monitor.subscribe(ApplicationStarted) {
+        connectionManager.start()
         resultConsumer.start()
     }
 
     monitor.subscribe(ApplicationStopping) {
+        connectionManager.stop()
         resultConsumer.stop()
     }
 }
