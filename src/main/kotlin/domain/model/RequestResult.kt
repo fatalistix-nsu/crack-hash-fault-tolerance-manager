be4eec93cf49e086ea1e7bc020fe0cf02bd1e106
Viacheslav Balashov <slavabal11@outlook.com>
2025-05-15 01:34:30 +0700

    referencing to request for persistance

--- src/main/kotlin/domain/model/RequestResult.kt
+++ src/main/kotlin/domain/model/RequestResult.kt
@@ -1,7 +1,7 @@
 package com.github.fatalistix.domain.model
 
 data class RequestResult(
-    val requestId: String,
     var status: RequestStatus = RequestStatus.IN_PROGRESS,
     val data: MutableSet<String> = mutableSetOf(),
+    val request: Request,
 )
