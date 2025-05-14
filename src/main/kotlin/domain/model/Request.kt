0a63455febe7f70f7421a131e92389c3c80add23
Viacheslav Balashov <slavabal11@outlook.com>
2025-05-15 01:34:12 +0700

    long usage

--- src/main/kotlin/domain/model/Request.kt
+++ src/main/kotlin/domain/model/Request.kt
@@ -6,13 +6,13 @@ data class Request(
     val id: String,
     val alphabet: String,
     val hash: String,
-    val maxLength: ULong,
+    val maxLength: Long,
 ) {
     val size = lazy {
-        var result = 0UL
-        val length = alphabet.length.toULong()
-        for (i in 0UL..<maxLength) {
-            result += length.pow(i+1UL)
+        var result = 0L
+        val length = alphabet.length.toLong()
+        for (i in 0L..<maxLength) {
+            result += length.pow(i+1L)
         }
 
         result
